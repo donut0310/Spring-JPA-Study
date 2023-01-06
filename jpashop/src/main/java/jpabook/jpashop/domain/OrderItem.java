77d979e66f19affc26e8b886a7be73fcec7b78d7
GeomJa <52377043+donut0310@users.noreply.github.com>
2023-01-06 11:25:18 +0900

    [feat] 주문 도메인 개발
    - 주문, 주문상품 엔티티(비즈니스, 생성 메서드 추가)
    - 주문 리포지토리
    - 주문 서비스
    - 주문 기능 테스트

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 객체를 직접 생성하지 않고 생성 메서드를 사용하도록 제약
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -23,4 +26,30 @@ public class OrderItem {
     private Order order;
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 재고 감소
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
